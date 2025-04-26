146499e88654b0cafa95bbf34e8b4fe9e01aca89
Ahmad Iman <ahmadimanh3@gmail.com>
2025-04-26 21:56:05 +0800

    change import libraries

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import streamlit as st
-from google.transit import gtfs_realtime_pb2
-from google.protobuf.json_format import MessageToDict
 import pandas as pd
 from requests import get
 import requests
